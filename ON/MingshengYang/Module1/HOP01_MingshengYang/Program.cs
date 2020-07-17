0f06cb887a45aedb1a2de468850dd3fa80e8c9a2
Young Yang <v-mingsy@microsoft.com>
2020-07-17 16:06:46 -0700

    Module 1 :  HOP1_Mingsheng Yang

---
+++ ON/MingshengYang/Module1/HOP01_MingshengYang/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using static System.Console;
+
+namespace HOP01_MingshengYang
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Mingsheng Yang";
+            int age = 29;
+            bool isFemale = false;
+            float myFolat = 10.1f;
+            WriteLine(name);
+            WriteLine(age);
+            WriteLine(isFemale);
+            WriteLine(myFolat);
+        }
+    }
+}
