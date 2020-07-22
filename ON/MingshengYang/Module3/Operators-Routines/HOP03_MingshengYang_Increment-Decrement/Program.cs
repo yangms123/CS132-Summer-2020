9cd3771cef377484ed045cfc31cd1e8ba15d678a
Young Yang <v-mingsy@microsoft.com>
2020-07-22 12:23:53 -0700

    this commit is for HOP03

---
+++ ON/MingshengYang/Module3/Operators-Routines/HOP03_MingshengYang_Increment-Decrement/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace HOP03_MingshengYang_Increment_Decrement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // output the value of a:  a started as 0 and increase 1 each time until it reaches 10 (include 10)
+            //for (int a = 0; a <= 10; a++)
+            //{
+            //    Console.WriteLine(a);
+            //}
+
+            for (int a = 15; a >= 1; a--)
+            {
+                Console.WriteLine(a);
+            }
+            
+        }
+    }
+}
---
+++ ON/MingshengYang/Module3/Operators-Routines/HOP03_MingshengYang_NumericOperators/HOP03_MingshengYang_NumericOperators/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using static System.Console;
+
+namespace HOP03_MingshengYang_NumericOperators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = 20;
+            int num2 = 77;
+            float num3 = 5.2f;
+
+            float sum = num1 + num2 + num3; //sum = 20 +77 + 5.2
+            int subtract = num2 - num1; // subtract = 77 - 20
+            int muliply = num1 * num2;  // muliply = 20 *77
+            float divide = num1 / num3;// divide = 20 / 5.3
+            int modulus = num2 % num1;// modulus = 77 % 20 
+
+            // output the value of each variable.
+            WriteLine("Sum of 3 numbers is: " + sum);
+            WriteLine("77 - 20 is: " + subtract);
+            WriteLine("20 * 77 is: " + muliply);
+            WriteLine("20 / 5.2 is: " + divide);
+            WriteLine("77 % 20 is: " + modulus);
+            ReadLine();
+
+        }
+    }
+}
