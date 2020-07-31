19cf123ec93695a78e056ede21f387a61f0ce497
Young Yang <v-mingsy@microsoft.com>
2020-07-31 13:09:01 -0700

    HOP04

---
+++ ON/MingshengYang/Module4/Methods-Scopes/HOP04_MingshengYang_01_Methods/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace HOP04_MingshengYang_01_Methods
+{
+    class Program
+    {
+        //public static void PrintSomething()
+        //{
+        //    Console.WriteLine("Hello World!");
+        //}
+
+        public static int Adding(int num1, int num2)
+        {
+            int sum = num1 + num2;
+            return sum;
+        }
+
+        public static int Mod(int a, int b)
+        {
+            return a % b;
+        }
+        static void Main(string[] args)
+        {
+            //PrintSomething();
+            //Console.WriteLine(Adding(10,20));
+            Console.WriteLine(Mod(20, 13));
+
+        }
+    }
+}
---
+++ ON/MingshengYang/Module4/Methods-Scopes/Scopes/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Scopes
+{
+    class Program
+    {
+        public static string Str2;
+        static void ReturnString(string Str1)
+        {
+            Str2 = "Programming in C#";
+            Console.Write(Str1);
+            //Console.WriteLine(Str2);
+        }
+        static void Main(string[] args)
+        {
+            ReturnString("CS132 ");
+            Console.WriteLine(Str2);
+        }
+    }
+}
+
