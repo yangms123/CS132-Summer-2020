561bd269d2faddf74fcbb7c4bbe5c26833a32612
Young Yang <v-mingsy@microsoft.com>
2020-08-06 12:58:23 -0700

    HOP05

---
+++ ON/MingshengYang/Module5/Conditions-Loops/Conditional-Statements/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Conditional_Statements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 1;
+            Console.WriteLine("Guess a number (1 - 10): ");
+            int userInput = Convert.ToInt32(Console.ReadLine());// receive user input and store in "userInput" variable
+            if (userInput < 1 || userInput > 10)
+            {
+                Console.WriteLine("Number must be between 1 - 10 !!!");
+            }
+            else if (userInput == num)
+            {
+                Console.WriteLine("Your guess was correct!");
+
+            }
+            else if (userInput != num)
+            {
+                Console.WriteLine("You guessed wrong. Goodluck next time!");
+            }
+        }
+    }
+}
---
+++ ON/MingshengYang/Module5/Conditions-Loops/While-Loop/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace While_Loop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 1;
+            Console.WriteLine("Guess a number (1 - 10): ");
+            int userInput = Convert.ToInt32(Console.ReadLine());// receive user input and store in "userInput" variable
+            while (true)
+            {
+                if (userInput > 0 && userInput <= 10)
+                {
+                    if (userInput == num)
+                    {
+                        Console.WriteLine("Your guess was correct!");
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine("You guessed wrong. Guess again: ");
+                        userInput = Convert.ToInt32(Console.ReadLine());
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Number must be between 1 – 10, guess again: ");
+                    userInput = Convert.ToInt32(Console.ReadLine());
+                }
+            }
+            
+
+        }
+    }
+}
