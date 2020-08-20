363c27e623a54a1b15d72af8e80483385d49f9f5
Young Yang <v-mingsy@microsoft.com>
2020-08-20 09:45:56 -0700

    HOP07

---
+++ ON/MingshengYang/Module7/DateTime/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using static System.Console;
+
+namespace Date_Time
+{
+    class Program
+    {
+        void ShowTime()
+        {
+            DateTime currentDate = DateTime.Now;
+            bool leapYear = DateTime.IsLeapYear(2020);
+            WriteLine(currentDate);
+            Write("2020 is a leapYear: ");
+            WriteLine(leapYear);
+        }
+        void GetpersonalInfo()
+        {
+            WriteLine("Enter your name: ");
+            string name = ReadLine();
+            WriteLine("What year are you born?:");
+            int birthYear = Convert.ToInt32(ReadLine());
+            int age = Convert.ToInt32(DateTime.Now.Year) - birthYear;
+
+            WriteLine("Here are what we know from your inputs: ");
+            WriteLine("Your name is "+ name);
+            WriteLine("Today is : "+ DateTime.Now.ToShortDateString());
+            WriteLine("Your age :" +age);
+        }
+        void GetMonthAndDays()
+        {
+            int mon = DateTime.Now.Month;
+            int year = DateTime.Now.Year;
+            WriteLine(mon);
+            WriteLine(DateTime.DaysInMonth(year,mon));
+        }
+        static void Main(string[] args)
+        {
+            //create an object of class Program to access a method of that class, in this case, is the ShowTime() method
+            Program MyProgram = new Program();
+            //MyProgram.ShowTime();
+            //MyProgram.GetpersonalInfo();
+            MyProgram.GetMonthAndDays();
+
+        }
+
+    }
+}
